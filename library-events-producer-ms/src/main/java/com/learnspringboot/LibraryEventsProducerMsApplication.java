12835f16663bf2f59df9bf20586564babf421529
Swaroop Vincent <swaroopvincent@gmail.com>
2021-10-09 14:42:11 +0100

    adding library-event-producer-ms to root project

---
+++ library-events-producer-ms/src/main/java/com/learnspringboot/LibraryEventsProducerMsApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsProducerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryEventsProducerMsApplication.class, args);
+    }
+
+}
